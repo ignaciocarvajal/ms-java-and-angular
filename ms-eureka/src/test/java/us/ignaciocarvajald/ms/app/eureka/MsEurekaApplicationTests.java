9c13d50ea598eb03aa8c12d7b860dcae157e28d0
Ignacio Carvajal Duran <ignaciocarvajald@gmail.com>
2020-04-18 01:02:12 -0400

    ms eureka server

---
+++ ms-eureka/src/test/java/us/ignaciocarvajald/ms/app/eureka/MsEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package us.ignaciocarvajald.ms.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
