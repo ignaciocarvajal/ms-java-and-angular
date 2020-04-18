fd5e7c1206a461406ec960d17ed2291a37a6ba07
Ignacio Carvajal Duran <ignaciocarvajald@gmail.com>
2020-04-18 15:39:01 -0400

    add common repository to ms-students

--- ms-users/src/main/java/us/ignaciocarvajald/ms/app/users/controllers/StudentController.java
+++ ms-users/src/main/java/us/ignaciocarvajald/ms/app/users/controllers/StudentController.java
@@ -5,42 +5,17 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import us.ignaciocarvajald.ms.app.users.models.entity.Student;
 import us.ignaciocarvajald.ms.app.users.services.StudentService;
+import us.ignaciocarvajald.ms.commons.controllers.CommonController;
 
 @RestController
-public class StudentController {
-	@Autowired
-	private StudentService service;
-
-	@GetMapping
-	public ResponseEntity<?> getStudents() {
-		return ResponseEntity.ok().body(service.findAll());
-	}
-
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getStudent(@PathVariable Long id) {
-		Optional<Student> studentData = service.findById(id);
-		if (studentData.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		return ResponseEntity.ok().body(studentData.get());
-	}
-
-	@PostMapping
-	public ResponseEntity<?> create(@RequestBody Student student) {
-
-		Student studentSaved = service.save(student);
-		return ResponseEntity.status(HttpStatus.CREATED).body(studentSaved);
-	}
+public class StudentController extends CommonController<Student, StudentService> {
 
 	@PutMapping("/{id}")
 	public ResponseEntity<?> modify(@RequestBody Student student, @PathVariable Long id) {
@@ -57,10 +32,5 @@ public class StudentController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(studentStorage));
 
 	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> delete(@PathVariable Long id){
-		service.deleteById(id);
-		return ResponseEntity.noContent().build();
-	}
+
 }
--- ms-users/src/main/java/us/ignaciocarvajald/ms/app/users/services/StudentServiceImpl.java
+++ ms-users/src/main/java/us/ignaciocarvajald/ms/app/users/services/StudentServiceImpl.java
@@ -1,11 +1,6 @@
 package us.ignaciocarvajald.ms.app.users.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import us.ignaciocarvajald.ms.app.users.models.entity.Student;
 import us.ignaciocarvajald.ms.app.users.models.repository.studentRepository;
